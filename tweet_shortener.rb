f5b8863f978cc708cac25abf12c126eaabdda54f
Peter Meister <prmeister89@gmail.com>
2018-07-11 23:55:14 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,8 +1,13 @@
+require 'pry'
+
 def dictionary
-  {
+  dictionary = {
   "hello" => 'hi',
-  "to, two, too" => '2',
-  "for, four" become '4',
+  "to" => '2',
+  "two" => '2',
+  "too" => '2',
+  "for" => '4',
+  "four" => '4',
   'be' => 'b',
   'you' => 'u',
   "at" => "@",
@@ -11,9 +16,39 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  new_array = tweet.split(" ")
-  value_array = tweet.values
-  binding.pry
-  tweet.each do |key, value|
-    
+  
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+
+def bulk_tweet_shortener(array_of_tweets)
+  array_of_tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+
+def selective_tweet_shortener(tweet)
+  
+  if tweet.length > 141
+    return word_substituter(tweet)
+  else 
+    tweet
+  end
+end
+
+
+def shortened_tweet_truncator(tweet)
+  
+  if word_substituter(tweet).length >= 140
+    word_substituter(tweet)[0..136] + "..."
+  else
+    tweet
+  end
 end
