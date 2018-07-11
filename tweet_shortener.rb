8823306d112985d40c4bbf463927659526ee992f
Peter Meister <prmeister89@gmail.com>
2018-07-11 21:29:14 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def dictionary
+  {
+  "hello" => 'hi',
+  "to, two, too" => '2',
+  "for, four" become '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@",
+  "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  new_array = tweet.split(" ")
+  value_array = tweet.values
+  binding.pry
+  tweet.each do |key, value|
+    
+end
